027c7a47967f23f8bf3470241da6aa820a3f4088
Ben deLong <bendelong45@gmail.com>
2019-02-24 12:14:12 -0600

    Updates

---
+++ precache-manifest.35849f016514fbdb61454abed8ac929a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40d031b1d6c641f1ada3",
+    "url": "/50-Shades-of-Grey-Click-Game/static/js/runtime~main.40d031b1.js"
+  },
+  {
+    "revision": "940d9a44457b0fcbdd88",
+    "url": "/50-Shades-of-Grey-Click-Game/static/js/main.940d9a44.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/50-Shades-of-Grey-Click-Game/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "940d9a44457b0fcbdd88",
+    "url": "/50-Shades-of-Grey-Click-Game/static/css/main.01645c90.chunk.css"
+  },
+  {
+    "revision": "97fa90a4e7536c3f2e07137bc5f04b67",
+    "url": "/50-Shades-of-Grey-Click-Game/index.html"
+  }
+];
